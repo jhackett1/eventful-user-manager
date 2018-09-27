d231171e5cb28ed2ca1d6766794d348b222346dd
jhackett1 <joshua.hackett93@gmail.com>
2018-09-27 15:36:06 +0100

    More actions available, and colourful console logs

--- index.js
+++ index.js
@@ -1,29 +1,96 @@
+const colors = require('colors')
 const admin = require('firebase-admin')
-
-const serviceAccount = require('../../serviceAccountKey.json');
+const serviceAccount = require('./serviceAccountKey.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-});
+})
 
-if (process.argv.length !== 3) {
-  throw Error('Invalid use of promote. Usage: node promote.js <email>');
+function malformed(){
+    throw Error('Invalid use of user manager. Usage: npm run demote | promote | check <email>')
 }
-const email = process.argv[2];
-grantModeratorRole(email).then((res) => {
-  console.log(`User ${email} has been given moderator role`);
-  process.exit(0);
-}).catch((err) => {
-  console.log('Failed to grant user moderator role: ' + err);
-  process.exit(1);
-});
 
-async function grantModeratorRole(email){
-    const user = await admin.auth().getUserByEmail(email);
-    if (user.customClaims && user.customClaims.admin === true) {
-      return;
+// Stop here if the command isn't properly formed
+if (process.argv.length !== 4) {
+    malformed()
+}
+
+// Deconstruct the command
+const verb = process.argv[2]
+const email = process.argv[3]
+
+if(verb == 'promote'){
+    // PROMOTE
+    // Set the organiser claim to true, creating it if it doesn't exist
+    addOrganiserClaim(email)
+        .then((res) => {
+            console.log(`User ${email} has been given organiser role`.bgGreen.black)
+            process.exit(0)
+        })
+        .catch((err) => {
+            console.log(colors.bgRed.black('Failed to grant user organiser role: ' + err))
+            process.exit(1)
+        })
+    async function addOrganiserClaim(email){
+        const user = await admin.auth().getUserByEmail(email)
+        if (user.customClaims && user.customClaims.admin === true) {
+            return
+        }
+        return admin.auth().setCustomUserClaims(user.uid, {
+            organiser: true,
+        })
     }
-    return admin.auth().setCustomUserClaims(user.uid, {
-      moderator: true,
-    });
-  }
+} else if(verb == 'demote'){
+    // DEMOTE
+    // Set the organiser claim to false if it exists
+    removeOrganiserClaim(email)
+        .then((res) => {
+            console.log(`User ${email} has been demoted to delegate role`.bgGreen.black)
+            process.exit(0)
+        }).catch((err) => {
+            console.log(colors.bgRed.black('Failed to demote to delegate role: ' + err))
+            process.exit(1)
+        })
+    async function removeOrganiserClaim(email){
+        const user = await admin.auth().getUserByEmail(email)
+        return admin.auth().setCustomUserClaims(user.uid, {
+            organiser: false,
+        })
+    }
+} else if(verb == 'check'){
+    // CHECK
+    // Log the user's custom claims to the console. Change nothing.
+    checkClaims(email)
+        .then((res) => {
+            console.log(`User's custom claims are:`.blue)
+            console.log(colors.bgBlue.black(res))
+            process.exit(0)
+        })
+        .catch((err) => {
+            console.log(colors.bgRed.black('Failed to check user: ' + err))
+            process.exit(1)
+        })
+    async function checkClaims(email){
+        const user = await admin.auth().getUserByEmail(email)
+        return user.customClaims
+    }
+} else if(verb == 'revoke'){
+    // REVOKE
+    // Invalidate the user's session, forcing them to sign in again
+    admin.auth().getUserByEmail(email)
+        .then((user)=>{
+            admin.auth().revokeRefreshTokens(user.uid)
+                .then(() => {
+                    console.log("User's refresh token successfully revoked. They will have to sign in again.".bgGreen.black)
+                    process.exit(0)
+                })
+                .catch((err) => {
+                    console.log(colors.bgRed.black('Failed to revoke refresh token: ' + err))
+                    process.exit(1)
+                })
+        })
+
+} else {
+    malformed()
+}
+
--- readme.md
+++ readme.md
@@ -1,9 +1,15 @@
-Eventful User Manager
+Eventful user manager
 =====================
 
-Simple command line utility to give organiser (aka admin) permissions to Eventful users.
+Simple command line utility to manager roles of Eventful users.
 
-It uses the Firebase Admin SDK to add a custom claim to the data stored about a user.
+You can do four things:
+* **Promote** delegates to organisers (admins)
+* **Demote** organisers back to delegates
+* **Check** a user's current role without changing it
+* **Revoke** a user's refresh token, forcing them to sign in again
+
+It uses the Firebase Admin SDK to [add a custom claim](https://firebase.google.com/docs/auth/admin/custom-claims) to the data stored about a user.
 
 How to use it
 ------------
@@ -14,7 +20,7 @@ In your Firebase project, go to **Project settings > Service accounts**, and dow
 
 Put that file in the root folder of this utility and rename it to `serviceAccountKey.json`.
 
-Then, just run `npm run promote user@email.com`, replacing the email with the user's email address.
+Then, just run `npm run promote | demote | check | revoke user@email.com`, replacing the email with the user's email address and using the right verb for the action you want to perform.
 
 The utility will let you know if the attempt succeeded or not.
 
@@ -22,11 +28,15 @@ Things to remember
 ------------------
 
 * The user needs to have signed in once already, otherwise Firebase won't recognise the email as belonging to a user.
-* The user **won't immediately see or benefit from their raised permissions**. The token needs to be refreshed, either manually or by waiting for the token to expire and be refreshed. You should probably remind the user to sign out and back in to the app.
-* You can't demote users back down to delegate level using this. The easiest way is to delete their user from Firebase, then have them sign in from scratch.
+
+* The user **won't immediately see or benefit from new organiser permissions**. Their token needs to be refreshed, either manually or by waiting for the token to expire and be refreshed. You should probably remind the user to sign out and back in to the app. You can also revoke their token manually by running the `revoke` command from here.
+
+* Equally, when a user is demoted back down to delegate level, this will only take effect when their token expires. You can revoke their token manually with `revoke`.
 
 To do
 -----
 
-1. Add a command to check a user's status
-2. Add a command to demote a user back down to delegate level
+1. ~~Add a command to check a user's status~~ Done!
+2. ~~Add a command to demote a user back down to delegate level~~ Done!
+3. ~~Make tokens revokable~~ Done!
+4. Make it globally installable?
